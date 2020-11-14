60e7c47ede1bfc84d196aea347d37e01e672cd9f
JiHyeonMon <jhk8806@naver.com>
2020-11-15 04:31:10 +0900

    step/11

--- bak-step11/bak7568.py
+++ bak-step11/bak7568.py
@@ -1,28 +1,46 @@
 #7568
 #덩치
 
+num_student = int(input())
+student_list = []
+
+for _ in range(num_student):
+    weight, height = map(int, input().split())
+    student_list.append([weight, height])
+
+for i in student_list:
+    rank = 1
+    for j in student_list:
+        if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+    print(rank, end = " ")
+
+    
+'''
 n = int(input())
-dungchi = []
+w = []
+h = []
 answer = [0 for i in range(n)]
 cnt = 1
 
 for i in range(n):
-     w, h = map(int, input().split())
-     dungchi.append([w,h])
+     a, b = map(int, input().split())
+     w.append(a)
+     h.append(b)
 
 while n>0:
-     for i in range(len(dungchi)):
-          if dungchi[i][0] == max(dungchi)[0]:
-               if dungchi[i][1] == max(dungchi, key=lambda x: x[1])[1]:
-                    print(i)
+     for i in range(n):
+          if w[i] == max(w):
+               if h[i] == max(h):
                     answer[i] = cnt
                     cnt+=1
-                    dungchi[i] = [0,0]
+                    w[i]=0
+                    h[i]=0
                else:
                     answer[i] = cnt
-                    dungchi[i][0] = 0
-               continue
-               
+                    w[i] = 0
+               pass
      n-=1
 
 print(answer)
+'''
