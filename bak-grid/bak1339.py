5f4a931d265b593e3c2aadcd998ea41df1ee4e20
JiHyeonMon <jhk8806@naver.com>
2021-02-05 23:15:02 +0900

    grid

--- bak-grid/bak1339.py
+++ bak-grid/bak1339.py
@@ -1,10 +1,11 @@
 #1339
 #단어 수학
 
+import operator
 word = []
 alpha = {}
 k = 9
-answer =[]
+
 for i in range(int(input())):
      word.append(list(input()))
 
@@ -13,14 +14,15 @@ word.sort(key=len, reverse=True)
 for i in range(len(word)):
      for j in range(len(word[i])):
           if word[i][j] not in alpha:
-               alpha[word[i][j]] = k
-               k-=1
-               
-for i in range(len(word)):
-     tmp=0
-     for j in range(len(word[i])):
-          tmp += alpha[word[i][j]] * 10**(len(word[i])-j-1)
-     answer.append(tmp)
+               alpha[word[i][j]] = 10**(len(word[i])-j-1)
+          else:
+               alpha[word[i][j]] += 10**(len(word[i])-j-1)
+
+num = list(alpha.values())
+num.sort(reverse=True)
+
+for i in range(len(num)):
+     num[i]*=k
+     k-=1
      
-print(alpha)
-print(sum(answer))
+print(sum(num))
